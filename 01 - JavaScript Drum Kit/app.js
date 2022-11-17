01b0bc051862dd9ec941d2b422432691a708cd9b
eyadal <31968847+eyadal@users.noreply.github.com>
2022-11-17 11:33:06 +0100

    css styling, transition removed after pressed button, function created

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -1,8 +1,17 @@
-window.addEventListener('keydown', function (e) {
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-  if (!audio) {
-    return
-  }
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  if (!audio) return // Stop the function to running all together
+  audio.currentTime = 0 // rewind to the start
   audio.play()
-  console.log(audio)
-})
+  key.classList.add('playing')
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return // skip it if it's not a transform
+  this.classList.remove('playing')
+}
+
+const keys = document.querySelectorAll('.key')
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition))
+window.addEventListener('keydown', playSound)
